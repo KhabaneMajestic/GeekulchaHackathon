101b57728ec7008b12054a8f82e29ea72e4374f7
Elvis <elvist@khabane.co.za>
2017-09-28 16:18:36 +0200

    first commit

---
+++ src/main/java/kmc/hackaton/KMCTravel/KmcTravelApplication.java
@@ -0,0 +1,12 @@
+package kmc.hackaton.KMCTravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmcTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmcTravelApplication.class, args);
+	}
+}
